a0404d14047b1ca9f842fb9da787245e46c850ff
DiegoMarchini <131981756+DiegoMarchini@users.noreply.github.com>
2026-04-11 19:13:35 +0200

    migliorata lista storico partite

--- app/src/main/java/it/unipd/esp2526/marchini/simongame/Activity2.kt
+++ app/src/main/java/it/unipd/esp2526/marchini/simongame/Activity2.kt
@@ -5,26 +5,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import it.unipd.esp2526.marchini.simongame.ui.theme.SimonGameTheme
 
 class Activity2 : ComponentActivity() {
@@ -49,9 +61,6 @@ class Activity2 : ComponentActivity() {
     }
 }
 
-
-val testList = listOf("partita1","partita2","partita3","partita4","partita5","partita6",
-    "partita7","partita8","partita9","partita10","partita11","partita12")
 @Composable
 fun ScreenTwo(modifier: Modifier = Modifier, gamesHistory : List<String>){
 
@@ -67,9 +76,32 @@ fun ScreenTwo(modifier: Modifier = Modifier, gamesHistory : List<String>){
 
 @Composable
 fun GamesList(modifier: Modifier = Modifier, games : List<String>){
+
+    val orientation = LocalConfiguration.current.orientation
+
+    val titleBox = if(orientation == Configuration.ORIENTATION_LANDSCAPE) 0.3f
+    else 0.1f
+
+    Box(
+        modifier = Modifier
+            .fillMaxHeight(titleBox)
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.DarkGray),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            modifier = modifier,
+            textAlign = Center,
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Black,
+            text = stringResource(R.string.games_history)
+        )
+    }
+
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
+        contentPadding = PaddingValues(12.dp, 12.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ){
         items(games.reversed()){
@@ -83,21 +115,34 @@ fun GameStatsRow(game : String){
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .height(50.dp)
+            .background(Color.Gray, RoundedCornerShape(20)),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
-    ){
+        horizontalArrangement = Arrangement.SpaceBetween
+    )
+    {
+
+        Spacer(modifier = Modifier.weight(0.05f))
+
         Text(
+            modifier = Modifier.weight(0.1f),
             text = if(game.isNotBlank()){
                 (game.count { it == ' ' } + 1).toString()
             }
-            else "0"
+            else "0",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
         )
 
+        Spacer(modifier = Modifier.weight(0.1f))
+
         Text(
+            modifier = Modifier.weight(0.7f),
             text = game,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
+
+        Spacer(modifier = Modifier.weight(0.05f))
     }
 }
