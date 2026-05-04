bf14875b12d99e0bc5fd6e825a3a4700a620361f
DiegoMarchini <131981756+DiegoMarchini@users.noreply.github.com>
2026-05-04 13:30:14 +0200

    creato ExtendedFloatingActionButton e nuove stringhe

--- app/src/main/java/it/unipd/esp2526/marchini/simongame/GameHistoryActivity.kt
+++ app/src/main/java/it/unipd/esp2526/marchini/simongame/GameHistoryActivity.kt
@@ -20,6 +20,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Games
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -47,7 +52,15 @@ class GameHistoryActivity : ComponentActivity() {
 
         setContent {
             SimonGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    floatingActionButton = {
+                        ExtendedFloatingActionButton(
+                        onClick = { },
+                        icon = { Icon(Icons.Filled.Games, getString(R.string.new_game)) },
+                        text = { Text(getString(R.string.new_game))})
+                    }
+                ) { innerPadding ->
                     ScreenTwo(
                         modifier = Modifier
                             .fillMaxSize()
