e046acc233c8bc9ae8829d2f4e31c81b90dc6f9d
DiegoMarchini <131981756+DiegoMarchini@users.noreply.github.com>
2026-05-10 11:38:26 +0200

    aggiunte librerie per l'utilizzo di flussi (Flow) di dati

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation (libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
