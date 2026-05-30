519100e22ec11318a5d771a7388e148c098b15d8
DiegoMarchini <131981756+DiegoMarchini@users.noreply.github.com>
2026-05-30 12:05:57 +0200

    implementato handle per far resistere lo stato della partita al recupero di risorse di android, spostati gli stati del GameComputer nel viewmodel

--- app/src/main/java/it/unipd/esp2526/marchini/simongame/GameViewModel.kt
+++ app/src/main/java/it/unipd/esp2526/marchini/simongame/GameViewModel.kt
@@ -2,9 +2,12 @@ package it.unipd.esp2526.marchini.simongame
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
 import it.unipd.esp2526.marchini.simongame.audio.SoundSynthesizer
 import it.unipd.esp2526.marchini.simongame.data.GameDao
 import it.unipd.esp2526.marchini.simongame.data.GameEntity
@@ -26,40 +29,49 @@ enum class GameState {IDLE, COMPUTER_TURN, PLAYER_TURN, PAUSE, GAME_OVER}
 // serve a evitare di dover ripetere computazioni per activity che vengono distrutte e ricreate continuamente
 class GameViewModel(
     application : Application,
-    private val dao : GameDao
+    private val dao : GameDao,
+    private val savedStateHandle: SavedStateHandle
 ) : AndroidViewModel(application) {
 
     // scelto l'uso di StateFlow per l'osservazione di flussi di dati da rendere visibili poi all'UI
     // uso una variabile privata che il ViewModel manipola, e ne espongo il valore in lettura  all'UI attraverso una variabile pubblica
     // mantengo l'incapsulamento
 
+    // variabili private da passare al GameComputer per sfruttare il savedStateHandle
+    private val computerSequence : StateFlow<List<Int>> = savedStateHandle.getStateFlow("computer_sequence", emptyList())
+    private val computerIndex : StateFlow<Int> = savedStateHandle.getStateFlow("computer_index", 0)
+    private val playbackIndex : StateFlow<Int> = savedStateHandle.getStateFlow("playback_index", 0)
+
     // variabile per indicare il button considerato dal computer
-    private val _highlightIndex = MutableStateFlow<Int?>(null)
-    val highlightIndex : StateFlow<Int?> = _highlightIndex.asStateFlow()
+    val highlightIndex : StateFlow<Int?> = savedStateHandle.getStateFlow("highlight_index", null)
 
     // variabile per indicare la GameEntity restituita dalla selezione tramite Id
-    private val _selectedGame = MutableStateFlow<GameEntity?>(null)
-    val selectedGame = _selectedGame.asStateFlow()
+    //private val _selectedGame = MutableStateFlow<GameEntity?>(null)
+    val selectedGame = savedStateHandle.getStateFlow("selected_game", null)
 
     // variabile per rappresentare il corrente stato di gioco
-    private val _gameState = MutableStateFlow(GameState.IDLE)
-    val gameState : StateFlow<GameState> = _gameState.asStateFlow()
+    //private val _gameState = MutableStateFlow(GameState.IDLE)
+    val gameState : StateFlow<GameState> = savedStateHandle.getStateFlow("game_state", GameState.IDLE)
 
     // variabile per tenere traccia del punteggio della partita
-    private val _score = MutableStateFlow(0)
-    val score : StateFlow<Int> = _score.asStateFlow()
+    val score : StateFlow<Int> = savedStateHandle.getStateFlow("score", 0)
 
     // variabile che consente di tenere traccia e modificare la sequenza visualizzata nell'area di testo nell'activity di gioco
-    private val _userSequence = MutableStateFlow("")
-    val userSequence : StateFlow<String> = _userSequence.asStateFlow()
+    val userSequence : StateFlow<String> = savedStateHandle.getStateFlow("user_sequence", "")
 
     val buttonTexts = listOf("R", "G", "B", "C", "M", "Y") // lista di supporto
 
     val sound = SoundSynthesizer()
     val computer = GameComputer(
-        visibleFbAction = { index -> _highlightIndex.value = index}, // callback per consentire all'oggetto GameComputer di modificare la UI
+        visibleFbAction = { index -> savedStateHandle["highlight_index"] = index}, // callback per consentire all'oggetto GameComputer di modificare la UI
         soundFbAction = { index -> sound.playTone(index)}, // callback per consentire all'oggetto GameComputer di riprodurre i suoni
-        getState = {_gameState.value}
+        getState = {gameState.value},
+        getSequence = {computerSequence.value},
+        setSequence = {newList -> savedStateHandle["computer_sequence"] = newList},
+        getIndex = {computerIndex.value},
+        setIndex = {newIndex -> savedStateHandle["computer_index"] = newIndex},
+        getPbIndex = {playbackIndex.value},
+        setPbIndex = {newIndex -> savedStateHandle["playback_index"] = newIndex}
     )
 
     // variabile che consente la visualizzazione della lista lista di partite in GameHistoryActivity
@@ -72,44 +84,44 @@ class GameViewModel(
 
     fun startGame(){
         computer.resetSequence()
-        _score.value = 0
+        savedStateHandle["score"] = 0
         startComputerTurn()
     }
 
     // funzione per far iniziare il turno del computer (invocata in GameActivity)
     fun startComputerTurn(){
         viewModelScope.launch {
-            _gameState.value = GameState.COMPUTER_TURN
+            savedStateHandle["game_state"] = GameState.COMPUTER_TURN
             computer.extendSequence()
             computer.playSequence(800)
-            if(_gameState.value == GameState.COMPUTER_TURN)_gameState.value = GameState.PLAYER_TURN
+            if(gameState.value == GameState.COMPUTER_TURN) savedStateHandle["game_state"] = GameState.PLAYER_TURN
         }
     }
 
     // funzione per controllare la correttezza del button cliccato dall'utente
     fun checkMove(index : Int) {
-        if(_gameState.value != GameState.PLAYER_TURN) return // ignoro la pressione di tasti se non è il turno del giocatore
+        if(gameState.value != GameState.PLAYER_TURN) return // ignoro la pressione di tasti se non è il turno del giocatore
         sound.playTone(index)
         viewModelScope.launch{
-            _highlightIndex.value = index
+            savedStateHandle["highlight_index"] = index
             delay(500)
-            _highlightIndex.value = null
+            savedStateHandle["highlight_index"] = null
         }
         val pressedButton = buttonTexts[index]
-        _userSequence.value = if(_userSequence.value.isNotBlank()) " ${_userSequence.value}, $pressedButton"
+        savedStateHandle["user_sequence"] = if(userSequence.value.isNotBlank()) " ${userSequence.value}, $pressedButton"
                               else pressedButton
         when(computer.checkPlayerMove(index)){
             1 -> {} // bottone corretto, sequenza non finita, in attesa dell'utente
             0 -> { // bottone corretto, sequenza finita, inizia un nuovo turno
-                _score.value += 1
+                savedStateHandle["score"] = score.value + 1
                 viewModelScope.launch {
                     delay(1000)
-                    _userSequence.value = ""
+                    savedStateHandle["user_sequence"] = ""
                     startComputerTurn()
                 }
             }
             -1 -> { // bottone sbagliato, partita terminata
-                _gameState.value = GameState.GAME_OVER
+                savedStateHandle["game_state"] = GameState.GAME_OVER
                 endGame()
             }
         }
@@ -119,24 +131,24 @@ class GameViewModel(
     fun resetComputer(){computer.resetSequence()}
 
     fun pauseGame(){ // col controllo tra indice e score mi assicuro che nel lasso di tempo tra presentazione dell'ultimo elemento e passaggio al PLAYER_TURN, non si possa premere pausa
-        if(_gameState.value == GameState.COMPUTER_TURN && computer.getPlaybackIndex() < _score.value) _gameState.value = GameState.PAUSE
+        if(gameState.value == GameState.COMPUTER_TURN && computer.getPlaybackIndex() < score.value) savedStateHandle["game_state"] = GameState.PAUSE
     }
 
     fun resumeGame(){
         if(gameState.value == GameState.PAUSE){
             viewModelScope.launch{
-                _gameState.value = GameState.COMPUTER_TURN
+                savedStateHandle["game_state"] = GameState.COMPUTER_TURN
                 computer.playSequence(800)
-                if(_gameState.value == GameState.COMPUTER_TURN) _gameState.value = GameState.PLAYER_TURN
+                if(gameState.value == GameState.COMPUTER_TURN) savedStateHandle["game_state"] = GameState.PLAYER_TURN
             }
         }
     }
 
     fun endGame(){
-        if(_score.value == 0 && (_gameState.value == GameState.COMPUTER_TURN || _gameState.value == GameState.IDLE)) return
-        val finalSequence = computer.getSequence()
+        if(score.value == 0 && (gameState.value == GameState.COMPUTER_TURN || gameState.value == GameState.IDLE)) return
+        val finalSequence = computer.getColorSequence()
         val errorIndex = computer.getErrorIndex()
-        insertGame(GameEntity(score = _score.value , sequence = finalSequence, errorIndex = errorIndex))
+        insertGame(GameEntity(score = score.value , sequence = finalSequence, errorIndex = errorIndex))
     }
 
     // funzione per inserire una nuova partita (invocata alla chiusura di GameActivity)
@@ -146,7 +158,7 @@ class GameViewModel(
 
     // funzione per ottenere i dati relativi ad una partita indicandone l'ID (invocata in DetailActivity)
     fun getGameByID(gameId : Int) = viewModelScope.launch{
-        _selectedGame.value = dao.getGameByID(gameId)
+        savedStateHandle["selected_game"] = dao.getGameByID(gameId)
     }
 
 }
@@ -157,10 +169,11 @@ class GameViewModelFactory(
     private val application : Application,
     private val dao : GameDao
 ) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass : Class<T>) : T {
+    override fun <T : ViewModel> create(modelClass : Class<T>, extras : CreationExtras) : T {
         if(modelClass.isAssignableFrom(GameViewModel::class.java)){
+            val savedStateHandle = extras.createSavedStateHandle()
             @Suppress("UNCHECKED_CAST")
-            return GameViewModel(application, dao) as T
+            return GameViewModel(application, dao, savedStateHandle) as T
         }
         throw IllegalArgumentException("Classe ViewModel sconosciuta")
     }
--- app/src/main/java/it/unipd/esp2526/marchini/simongame/logic/GameComputer.kt
+++ app/src/main/java/it/unipd/esp2526/marchini/simongame/logic/GameComputer.kt
@@ -1,5 +1,6 @@
 package it.unipd.esp2526.marchini.simongame.logic
 
+import androidx.collection.MutableIntList
 import it.unipd.esp2526.marchini.simongame.GameState
 import kotlinx.coroutines.delay
 import kotlin.random.Random
@@ -8,37 +9,42 @@ import kotlin.random.Random
 class GameComputer(
     private val visibleFbAction: (Int?) -> Unit,
     private val soundFbAction: (Int?) -> Unit,
-    private val getState : () -> GameState
+    private val getState : () -> GameState,
+    private val getSequence : () -> List<Int>,
+    private val setSequence : (List<Int>) -> Unit,
+    private val getIndex : () -> Int,
+    private val setIndex : (Int) -> Unit,
+    private val getPbIndex : () -> Int,
+    private val setPbIndex : (Int) -> Unit
 ) {
     private val buttons = listOf("R", "G", "B", "C", "M", "Y")
-    private val sequence = mutableListOf<Int>()
-    private var currentIndex = 0
-    private var playbackIndex = 0
 
     // funzione che estende la sequenza di un button alla volta
     fun extendSequence() {
         val nextButton = Random.nextInt(buttons.size)
+        val sequence = getSequence().toMutableList()
         sequence.add(nextButton)
+        setSequence(sequence)
     }
 
-    fun getSequence() : String { return sequence.joinToString(", "){index -> buttons[index]} }
-
-    fun getErrorIndex() : Int { return currentIndex }
-    fun getPlaybackIndex() : Int { return playbackIndex}
+    fun getColorSequence() : String { return getSequence().joinToString(", "){index -> buttons[index]} }
+    fun getErrorIndex() : Int { return getIndex() }
+    fun getPlaybackIndex() : Int { return getPbIndex()}
     fun resetSequence() {
-        sequence.clear()
+        setSequence(emptyList())
     }
 
     // funzione che riproduce all'utente la sequenza con feedback visivi e uditivi
     // le funzioni usate per il feedback sono passate tramite parametro alla classe GameComputer
     suspend fun playSequence(pauseMillis : Long){
-        while(playbackIndex < sequence.size){
+        val sequence = getSequence()
+        while(getPbIndex() < sequence.size){
             if(getState() == GameState.PAUSE){
                 visibleFbAction(null)
                 //soundFbAction(null)
                 break
             }
-            val button = sequence[playbackIndex]
+            val button = sequence[getPbIndex()]
             visibleFbAction(button)
             soundFbAction(button)
             delay(pauseMillis)
@@ -46,17 +52,18 @@ class GameComputer(
             visibleFbAction(null)
             //soundFbAction(null)
             delay(pauseMillis/4) // pausa breve per impedire un feedback continuo in caso ci siano due colori uguali consecutivi nella sequenza
-            playbackIndex++
+            setPbIndex(getPbIndex() + 1)
         }
-        if(playbackIndex >= sequence.size) playbackIndex = 0 // finita la presentazione di una sequenza mi preparo al turno successivo
+        if(getPbIndex() >= sequence.size) setPbIndex(0) // finita la presentazione di una sequenza mi preparo al turno successivo
     }
 
     fun checkPlayerMove(playerIndex : Int) : Int {
         var feedback = 1
-        if(playerIndex == sequence[currentIndex]) {
-            currentIndex++
-            if (currentIndex >= sequence.size){
-                currentIndex = 0
+        val sequence = getSequence()
+        if(playerIndex == sequence[getIndex()]) {
+            setIndex(getIndex() + 1)
+            if (getIndex() >= sequence.size){
+                setIndex(0)
                 feedback = 0
             }
         }
